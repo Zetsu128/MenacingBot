c62c2b36fad52f1299f5eca4069ef7a8d4d8d2e9
Zetsu128 <vaughnwoerpel@gmail.com>
2017-12-14 14:40:13 -0500

    First Commit

---
+++ MenacingBotNet4.6.1/MenacingBotNet4.6.1/Modules/Ping.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenacingBotNet4._6._1.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync(string name)
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            /*builder.AddField("\n\nField1", "test")
+                .AddInlineField("Field2", "test")
+                .AddInlineField("Field3", "test");
+
+            
+            builder.WithColor(Color.Purple);
+
+            Context.User;
+            Context.Client;
+            Context.Guild;
+
+            
+
+            await ReplyAsync("", false, builder.Build()); 
+            */
+
+            await ReplyAsync($"{name} is a noob");
+        }
+    }
+}
---
+++ MenacingBotNet4.6.1/MenacingBotNet4.6.1/Modules/Skills.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenacingBotNet4._6._1.Modules
+{
+    public class Skills : ModuleBase<SocketCommandContext>
+    {
+        [Command("skills")]
+        public async Task SkillsAsync(string name)
+        {
+            Random rnd = new Random();
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            /*builder.AddField("\n\nField1", "test")
+                .AddInlineField("Field2", "test")
+                .AddInlineField("Field3", "test");
+
+           
+            builder.WithColor(Color.Purple);
+
+            await ReplyAsync("", false, builder.Build()); 
+            */
+
+            builder.WithColor(Color.Purple);
+
+            int skills = rnd.Next(0, 10);
+            await ReplyAsync($"Hmmm... I'd rate {name}'s skills a {skills}/10");
+
+        }
+    }
+}
